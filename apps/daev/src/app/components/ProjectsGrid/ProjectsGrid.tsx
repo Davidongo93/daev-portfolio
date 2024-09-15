089a32917e7cd5f2e6be5d3a34e93e13c506d290
David Orlando Miranda Roa <domirandar@unal.edu.co>
2024-09-15 00:06:33 -0500

    Feat: frontend console view component

--- apps/daev/src/app/components/ProjectsGrid/ProjectsGrid.tsx
+++ apps/daev/src/app/components/ProjectsGrid/ProjectsGrid.tsx
@@ -37,16 +37,24 @@ const getRandomColorClass = () => {
   return `bg-${randomColor}-${shade}`;
 };
 
+// Define the Repo interface
+interface Repo {
+  id: number;
+  description: string | null;
+  html_url: string;
+  language: string | null;
+}
+
 const CardGrid = () => {
-  const [repos, setRepos] = useState([]);
-  const [cardColors, setCardColors] = useState([]);
+  const [repos, setRepos] = useState<Repo[]>([]); // Use Repo[] type for the repos array
+  const [cardColors, setCardColors] = useState<string[]>([]); // Colors for the cards
 
   useEffect(() => {
     // Fetch repositories from GitHub API
     fetch('https://api.github.com/users/Davidongo93/repos')
       .then((response) => response.json())
-      .then((data) => {
-        setRepos(data);
+      .then((data: Repo[]) => {
+        setRepos(data); // Use the Repo type to ensure correct structure
         const colors = data.map(() => getRandomColorClass());
         setCardColors(colors);
       })
@@ -57,16 +65,16 @@ const CardGrid = () => {
     <div className="shadow border-4 rounded mx-auto px-4 py-8">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-6 gap-4">
         {repos.map((repo, index) => (
-          <Link href={repo.url} key={repo.id}>
-            <div className={`p-4 rounded shadow border ${cardColors[index]} w-auto h-auto`}>
-              <div className="text-center text-black">
+          <Link href={repo.html_url} key={repo.id}>
+            <div className={`list-item-link p-4 rounded shadow border ${cardColors[index]} w-auto h-auto`}>
+              <span className="text-center text-black">
                 <h2 className="text-bold">
                   {repo.description || 'No description'}
                 </h2>
                 <p className="text-black text-sm italic">
                   Language: {repo.language || 'Unknown'}
                 </p>
-              </div>
+              </span>
             </div>
           </Link>
         ))}
--- apps/daev/src/app/page.tsx
+++ apps/daev/src/app/page.tsx
@@ -1,14 +1,19 @@
 'use client'
 
+import ConsoleCLI from "./components/console/ConsoleCLI";
 import CardGrid from "./components/ProjectsGrid/ProjectsGrid";
 
 export default function Index() {
 
   return (
-    <div>
-      <div className="wrapper">
-      <CardGrid/>   
-      </div>
-    </div>
+      <ConsoleCLI/>
+    // <div>
+    //   <div className="wrapper">
+    //     <div className="container">
+    //       <div ></div>
+    //     </div>
+    //   <CardGrid/>   
+    //   </div>
+    // </div>
   );
 }

17af0fe5f4808c04465dcd2fb1aa7d072bf5bbf5
David Orlando Miranda Roa <domirandar@unal.edu.co>
2024-09-16 14:14:14 -0500

    fix: types childprops terminalButton

--- apps/daev/src/app/components/TerminalButton/TerminalButton.tsx
+++ apps/daev/src/app/components/TerminalButton/TerminalButton.tsx
@@ -1,6 +1,9 @@
 'use client';
 import { FaTerminal } from 'react-icons/fa'; 
 
+interface ChildComponentProps {
+  onStateChange: (newValue: boolean) => void;
+}
 
 // El componente maneja el estado de cli
 const TerminalButton: React.FC<ChildComponentProps> = ({ onStateChange }) => {

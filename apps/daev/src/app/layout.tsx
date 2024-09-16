10426034e59f661c077595918d6e67aeb2369dde
David Orlando Miranda Roa <domirandar@unal.edu.co>
2024-09-16 14:42:58 -0500

    Feat: google analytics

--- apps/daev/src/app/layout.tsx
+++ apps/daev/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import './global.css';
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata = {
   title: 'Welcome to daev',
@@ -14,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
        <GoogleTagManager gtmId="GTM-K5PFW7FW" />
+       <GoogleAnalytics gaId="G-7LTKWZT49T" />
       <body>{children}</body>
     </html>
   );
